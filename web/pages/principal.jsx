6e70b04e892354058806158b218849157d06ee5e
Abed Llanos <negolloverallanos@gmail.com>
2025-09-20 23:57:54 -0500

    actualizacion de package json para funcioanidad tailwind

---
+++ web/pages/principal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Bienvenida extends React.Component {
+    render() {
+        return (
+            <div className=" align-center text-center p-20">
+                <h1 className='text-red-700'>Welcome to Juan</h1>
+            </div>
+        )
+    }
+}
+
+export default Bienvenida;
