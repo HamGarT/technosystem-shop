74a0dd9910f3cd7f13383731437ba2d2b412b6f3
Abed Llanos <negolloverallanos@gmail.com>
2025-09-20 22:26:43 -0500

    home home

---
+++ web/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="bg-gray-800 align-center text-center p-20">
+                <h1>Welcome to Dashboard</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
