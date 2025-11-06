ae734751a61a6941b16e0e62743a498973e054a3
Hamer Garcia <hamgar28@gmail.com>
2025-11-06 15:11:49 -0500

    deploy 2025-11-06 vite configuration were causing errors

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: '0.0.0.0', // ¡Esto es lo que falta!
-    port: 3000
-  }
+  // server: {
+  //   host: '0.0.0.0', // ¡Esto es lo que falta!
+  //   port: 3000
+  // }
 })
